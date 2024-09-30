ec6f7edcb05e6c8c17eae94dbef04f92249b3ff1
softpromani <softpromani@gmail.com>
2024-09-30 21:22:16 +0530

    ui and backend changes

--- resources/views/user/works.blade.php
+++ resources/views/user/works.blade.php
@@ -49,14 +49,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-5.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-18.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">ITrove Food Delivery System</a></h3>
+                                    <h3><a href="#">ITrove Food Delivery System</a></h3>
                                     <p>A food delivery system is a service or platform that facilitates the ordering and delivery of food from restaurants or food outlets to customers. These systems have become increasingly popular, offering convenience for consumers and expanded customer reach for food businesses.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -67,14 +67,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-6.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-19.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">ITrove ERP</a></h3>
+                                    <h3><a href="#">ITrove ERP</a></h3>
                                     <p>ERP (Enterprise Resource Planning) system, possibly using "smothing" as a placeholder or generic term. ERP systems are comprehensive software platforms used by organizations to manage and integrate important parts of their businesses.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -85,14 +85,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-11.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-16.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">Theorie Stampers</a></h3>
+                                    <h3><a href="#">Theorie Stampers</a></h3>
                                     <p>An online driving exam system represents a significant step toward modernizing the driver's licensing process, making it more efficient, accessible, and adaptable to the needs of a diverse population.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -103,14 +103,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-12.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-18.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">Grievance Portal</a></a></h3>
+                                    <h3><a href="#">Grievance Portal</a></a></h3>
                                     <p>A grievance portal is an online platform designed to manage and resolve complaints or grievances raised by employees, customers, citizens, or any specific group of users. These portals are widely used in various sectors, including corporate, government, educational institutions, and public service organizations. The primary goal is to provide a transparent, accessible, and efficient means of addressing concerns, ensuring that grievances are heard, processed, and resolved in a fair and timely manner.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -121,15 +121,15 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-13.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-19.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">File Tracking System</a></h3>
+                                    <h3><a href="#">File Tracking System</a></h3>
                                     <p>
                                         A File Tracking System is a digital solution designed to track the movement, status, and management of files or documents within an organization. This system is especially useful in environments where numerous documents are processed and require approval from multiple departments or individuals, such as government offices, legal firms, large corporations, and educational institutions. By digitizing the tracking process, organizations can significantly improve efficiency, accountability, and transparency in their document handling processes.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -140,14 +140,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-13.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-19.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">Pizza Paradise</a></h3>
+                                    <h3><a href="#">Pizza Paradise</a></h3>
                                     <p>This system has gained immense popularity due to its convenience, variety, and the ability to order easily through websites or mobile apps.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
@@ -158,14 +158,14 @@
                             <div class="inner-box">
                                 <figure class="image-box">
                                     <img src="{{ asset('frontend/assets/images/gallery/project-13.jpg')}}" alt="">
-                                    <a href="project-details.html" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
+                                    <a href="#" class="link"><i class="fas fa-expand-arrows-alt"></i></a>
                                 </figure>
                                 <div class="lower-content">
                                     <figure class="icon-box"><img src="{{ asset('frontend/assets/images/icons/icon-19.png')}}" alt=""></figure>
-                                    <h3><a href="project-details.html">Meencat PC</a></h3>
+                                    <h3><a href="#">Meencat PC</a></h3>
                                     <p>To create an interactive, user-friendly digital learning platform that enhances the educational experience for students and facilitates efficient teaching methodologies for educators.</p>
                                     <div class="btn-box">
-                                        <a href="project-details.html" class="theme-btn btn-eight">Read More</a>
+                                        <a href="#" class="theme-btn btn-eight">Read More</a>
                                     </div>
                                 </div>
                             </div>
